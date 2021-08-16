f6ce8a016adb53b00e40c4cad8bf5621e6e9072d
Kevin Jo <kevinhjo725@gmail.com>
2021-08-16 13:28:34 -0400

    Remove keys

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://dev:2TAHDzwYYutIQUaP@cluster0.gyvnb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-              //Make sure this is your own unique string
-  }
