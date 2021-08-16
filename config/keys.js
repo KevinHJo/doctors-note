1896efb0fb99a374fe17a71a41f0ff9dd92564ca
Kevin Jo <kevinhjo725@gmail.com>
2021-08-16 13:24:33 -0400

    Set up mongodb

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dev:2TAHDzwYYutIQUaP@cluster0.gyvnb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+              //Make sure this is your own unique string
+  }
