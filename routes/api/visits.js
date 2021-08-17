b6d809f525dd12f1fd0211bd3ec49c16bc228d5a
Kevin Jo <kevinhjo725@gmail.com>
2021-08-17 18:04:31 -0400

    Finish backend routes for creating and updating visits

--- routes/api/visits.js
+++ routes/api/visits.js
@@ -15,18 +15,18 @@ router.post('/new', (req, res) => {
 				.catch(err => console.log(err));
 });
 
-router.post('/update', (req, res) => {
-  const visitId = req.body.id;
+router.patch('/update/:id', (req, res) => {
+  const visitId = req.params.id;
 	const subjective = req.body.subjective;
 	const objective = req.body.objective;
   const assessment = req.body.assessment;
 	const plan = req.body.plan;
 
-	Visit.updateOne({ id: visitId }, {
-    'subjective': subjective,
-    'objective': objective,
-    'assessment': assessment,
-    'plan': plan
+	Visit.updateOne({_id: visitId }, {
+    subjective,
+    objective,
+    assessment,
+    plan
   })
     .then(visit => res.json(visit))
     .catch(err => console.log(err));
