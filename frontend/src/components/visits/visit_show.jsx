bed335728f10402051e0465fa435953752a6c413
Kevin Jo <kevinhjo725@gmail.com>
2021-08-19 12:57:24 -0400

    Add nav bar to visit show

--- frontend/src/components/visits/visit_show.jsx
+++ frontend/src/components/visits/visit_show.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import VisitForm from './visit_form'
+import TopNavBarContainer from '../navbar/top_nav_bar_container'
 
 class VisitShow extends Component {
   constructor(props) {
@@ -25,35 +26,44 @@ class VisitShow extends Component {
       const {visit} = this.props
       if (this.state.editing) {
         return (
-          <VisitForm visit={visit} processForm={this.props.processForm} toggleEdit={this.toggleEdit}/>
+          <div>
+            <TopNavBarContainer />
+            <div id='spacer'/>
+            <VisitForm visit={visit} processForm={this.props.processForm} toggleEdit={this.toggleEdit}/>
+          </div>
+          
         )
       } else {
         return (
-        <div className='visit-show'>
-          <div className='soap-note'>
-            <div className='soap-subjective'>
-              <h1>Subjective</h1>
-              <div className='soap-subjective-display' dangerouslySetInnerHTML={{ __html: visit.subjective }}/>
-            </div>
+          <div>
+            <TopNavBarContainer />
+            <div id='spacer'/>
+            <div className='visit-show'>
+              <div className='soap-note'>
+                <div className='soap-subjective'>
+                  <h1>Subjective</h1>
+                  <div className='soap-subjective-display' dangerouslySetInnerHTML={{ __html: visit.subjective }}/>
+                </div>
 
-            <div className='soap-objective'>
-              <h1>Objective</h1>
-                <div className='soap-objective-display' dangerouslySetInnerHTML={{ __html: visit.objective }}/>
-            </div>
+                <div className='soap-objective'>
+                  <h1>Objective</h1>
+                    <div className='soap-objective-display' dangerouslySetInnerHTML={{ __html: visit.objective }}/>
+                </div>
 
-            <div className='soap-assessment'>
-              <h1>Assessment</h1>
-                <div className='soap-assessment-display' dangerouslySetInnerHTML={{ __html: visit.assessment }}/>
-            </div>
+                <div className='soap-assessment'>
+                  <h1>Assessment</h1>
+                    <div className='soap-assessment-display' dangerouslySetInnerHTML={{ __html: visit.assessment }}/>
+                </div>
 
-            <div className='soap-plan'>
-              <h1>Plan</h1>
-                <div className='soap-plan-display' dangerouslySetInnerHTML={{ __html: visit.plan }}/>
+                <div className='soap-plan'>
+                  <h1>Plan</h1>
+                    <div className='soap-plan-display' dangerouslySetInnerHTML={{ __html: visit.plan }}/>
+                </div>
+              </div>
+
+              <button onClick={this.toggleEdit}>Edit</button>
             </div>
           </div>
-
-          <button onClick={this.toggleEdit}>Edit</button>
-        </div>
       )
       }
     } else {
