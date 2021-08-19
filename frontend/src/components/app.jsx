aa6d442bf5766a55a1b0901bb4c84c4d2564a119
Kevin Jo <kevinhjo725@gmail.com>
2021-08-18 21:16:01 -0400

    Fix Visits index container

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -10,14 +10,14 @@ import HomePageContainer from './home/home_page_container';
 import PatientsIndexContainer from './patients/index_page_container';
 
 //TESTING - start
-import VisitsIndexContainer from './visits/visits_index_container';
+import VisitsIndex from './visits/visits_index';
 //TESTING -end
 
 const App = () => (
     <div>
         <Switch>
             {/* TESTING ROUTES */}
-            <ProtectedRoute exact path='/charts/:patientId/visits' component={VisitsIndexContainer} />
+            <ProtectedRoute exact path='/charts/:patientId/visits' component={VisitsIndex} />
             {/* END TESTING ROUTES */}
             <AuthRoute exact path="/" component={HomePageContainer} />
             <AuthRoute exact path="/patients/login" component={LoginPageContainer} />
